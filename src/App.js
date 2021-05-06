f56ecf09ea625aa36f47981256cb88fdc65d7ea8
hinoharashinya <hinoshin0817@yahoo.co.jp>
2021-05-07 07:32:23 +0900

    create project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <h1>TODOリスト</h1>
+      <div>
+        <h2>入力エリア</h2>
+        <input placeHolder="TODOを入力"/>
+        <button>追加</button>
+      </div>
+      <div>
+        <h2>未完了のTODO</h2>
+        <ul>
+          <li>TODO1</li>
+          <button>完了</button>
+          <button>削除</button>
+          <li>TODO2</li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+      </div>
+      <div>
+        <h2>完了のTODO</h2>
+          <ul>
+            <li>TODO3</li>
+            <button>戻す</button>
+            <li>TODO4</li>
+            <button>戻す</button>
+          </ul>
+        </div>
     </div>
   );
 }
