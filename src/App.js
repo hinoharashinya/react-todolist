4d653500beb855b54bfe4ad88a0ddefb98521dbb
hinoharashinya <hinoshin0817@yahoo.co.jp>
2021-05-07 07:52:06 +0900

    create add function

--- src/App.js
+++ src/App.js
@@ -1,32 +1,55 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [inputTodo, setInputTodo] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState(["test1","test2"]);
+  const [completeTodos, setCompleteTodos] = useState(["test3", "test4"]);
+
+  const onChangeInputTodo = (e) => {
+    setInputTodo(e.target.value);
+  }
+
+  const onClickAdd = () => {
+    if(inputTodo === "") return;
+    const newTodos = [...incompleteTodos, inputTodo];
+    setIncompleteTodos(newTodos);
+    setInputTodo("");
+  }
+
   return (
     <div className="App-header">
       <h1>TODOリスト</h1>
       <div>
         <h2>入力エリア</h2>
-        <input placeHolder="TODOを入力"/>
-        <button>追加</button>
+        <input placeHolder="TODOを入力" value={inputTodo} onChange={onChangeInputTodo}/>
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div>
         <h2>未完了のTODO</h2>
         <ul>
-          <li>TODO1</li>
-          <button>完了</button>
-          <button>削除</button>
-          <li>TODO2</li>
-          <button>完了</button>
-          <button>削除</button>
+          {incompleteTodos.map((todo) => {
+          return(
+            <div key={todo}>
+              <li>{todo}</li>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          )
+          })}
         </ul>
       </div>
       <div>
         <h2>完了のTODO</h2>
           <ul>
-            <li>TODO3</li>
-            <button>戻す</button>
-            <li>TODO4</li>
-            <button>戻す</button>
+            {completeTodos.map((todo) => {
+            return(
+              <div key={todo}>
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            )
+            })}
           </ul>
         </div>
     </div>
