ddd6c76ec59a62e14b5271fbcca35f3d043d6672
hinoharashinya <hinoshin0817@yahoo.co.jp>
2021-05-09 09:04:54 +0900

    create IncompleteTodos component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useState} from "react";
 import {InputTodo} from "./components/InputTodo";
+import { IncompleteTodos } from './components/IncompleteTodos';
 
 function App() {
   const [inputTodo, setInputTodo] = useState("");
@@ -48,20 +49,7 @@ function App() {
     <div className="App-header">
       <h1>TODOリスト</h1>
       <InputTodo inputTodo={inputTodo} onChangeInputTodo={onChangeInputTodo} onClickAdd={onClickAdd}/>
-      <div>
-        <h2>未完了のTODO</h2>
-        <ul>
-          {incompleteTodos.map((todo, index) => {
-          return(
-            <div key={todo}>
-              <li>{todo}</li>
-              <button onClick={() => onClickComplete(index)}>完了</button>
-              <button onClick={() => onClickDelete(index)}>削除</button>
-            </div>
-          )
-          })}
-        </ul>
-      </div>
+      <IncompleteTodos incompleteTodos={incompleteTodos} onClickComplete={onClickComplete} onClickDelete={onClickDelete} />
       <div>
         <h2>完了のTODO</h2>
           <ul>
---
+++ src/components/IncompleteTodos.jsx
@@ -0,0 +1,19 @@
+export const IncompleteTodos = (props) => {
+  const {incompleteTodos, onClickComplete, onClickDelete} = props;
+  return(
+    <div>
+        <h2>未完了のTODO</h2>
+        <ul>
+          {incompleteTodos.map((todo, index) => {
+          return(
+            <div key={todo}>
+              <li>{todo}</li>
+              <button onClick={() => onClickComplete(index)}>完了</button>
+              <button onClick={() => onClickDelete(index)}>削除</button>
+            </div>
+          )
+          })}
+        </ul>
+      </div>
+  );
+}
